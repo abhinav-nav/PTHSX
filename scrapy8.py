8a359939cfaaefa12384e6dc76351d770b7af97b
Abhinav Abhinav <99717375+abhinav-nav@users.noreply.github.com>
2023-06-15 23:58:36 +0530

    Create scrapy8.py

---
+++ scrapy8.py
@@ -0,0 +1,88 @@
+import requests
+import urllib.parse
+from bs4 import BeautifulSoup
+import hashlib
+from pykronalyze import analyze
+from pykronalyze.utils import BehaviorPrinter
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+# Define the URL for the machine learning model
+model_url = 'https://www.example.com/malware_detection_model.h5'
+
+def fetch_website_content(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.content
+        else:
+            print(f"Failed to fetch content for {url}")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+def extract_text(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    text = soup.get_text(separator=' ')
+    return text.strip()
+
+def calculate_md5_hash(content):
+    md5_hash = hashlib.md5(content).hexdigest()
+    return md5_hash
+
+def load_model_from_url(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            with open('malware_detection_model.h5', 'wb') as file:
+                file.write(response.content)
+        else:
+            print(f"Failed to fetch the model from {url}")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+def preprocess_text(text, tokenizer, max_length):
+    sequences = tokenizer.texts_to_sequences([text])
+    padded_sequences = pad_sequences(sequences, maxlen=max_length, padding='post', truncating='post')
+    return padded_sequences
+
+def analyze_website(url):
+    content = fetch_website_content(url)
+    if not content:
+        return
+
+    # Calculate the MD5 hash of the content
+    md5_hash = calculate_md5_hash(content)
+
+    # Extract text content from HTML
+    text_content = extract_text(content)
+
+    # Perform behavior analysis using PyKronalyze
+    behavior_result = analyze(text_content, config={'print': BehaviorPrinter})
+    if behavior_result:
+        print(f"Behavior analysis results for '{url}':")
+        behavior_result.print()
+
+    # Load the malware detection model
+    load_model_from_url(model_url)
+    model = load_model('malware_detection_model.h5')
+
+    if model:
+        # Preprocess the text content
+        tokenizer = Tokenizer()
+        tokenizer.fit_on_texts([text_content])
+        max_length = max([len(sequence.split()) for sequence in tokenizer.texts_to_sequences([text_content])])
+        preprocessed_text = preprocess_text(text_content, tokenizer, max_length)
+
+        # Predict malware probability
+        malware_probability = model.predict(preprocessed_text)[0][0]
+
+        if malware_probability >= 0.5:
+            print(f"The website '{url}' is classified as potential malware with a probability of {malware_probability}.")
+        else:
+            print(f"The website '{url}' is classified as benign with a probability of {1 - malware_probability}.")
+
+
+# Test the script
+website_url = 'https://www.example.com'
+analyze_website(website_url)
