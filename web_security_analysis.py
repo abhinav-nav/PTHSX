62fe210b3d083621e437d20d99e2ba108d3cb5d0
Abhinav Abhinav (anirudh) <99717375+abhinav-nav@users.noreply.github.com>
2023-09-25 10:31:54 +0530

    Create web_security_analysis.py
    In this advanced script, we've added the ability to perform SQL injection scans using sqlmap API. Additionally, we've included placeholders for further advanced security checks and analysis that you can implement based on your specific requirements.
    
    Please make sure to replace 'your_vulners_api_key' with your own API key obtained from the Vulners vulnerability database.

---
+++ web_security_analysis.py
@@ -0,0 +1,138 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse, urljoin
+import hashlib
+from cryptography.fernet import Fernet
+from concurrent.futures import ThreadPoolExecutor
+from vulners import Vulners
+from wafw00f import WafW00F
+from subbrute import SubBrute
+from sqlmap import api as sqlmap_api
+
+# Define the URL of the website to be scanned
+website_url = 'https://www.example.com'
+
+# Define the API key for the Vulners vulnerability database
+vulners_api_key = 'your_vulners_api_key'
+
+# Define the payload for testing XSS
+xss_payload = '<script>alert("XSS Vulnerability")</script>'
+
+def fetch_website_content(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.content
+        else:
+            print(f"Failed to fetch content for {url}")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+def extract_links(content, base_url):
+    soup = BeautifulSoup(content, 'html.parser')
+    links = []
+    for link in soup.find_all('a', href=True):
+        href = link['href']
+        if href.startswith('#'):
+            continue
+        parsed_url = urlparse(href)
+        if parsed_url.netloc == '':
+            link_url = urljoin(base_url, href)
+        else:
+            link_url = href
+        links.append(link_url)
+    return links
+
+def calculate_md5_hash(content):
+    md5_hash = hashlib.md5(content).hexdigest()
+    return md5_hash
+
+def encrypt_sensitive_data(data, encryption_key):
+    f = Fernet(encryption_key)
+    encrypted_data = f.encrypt(data.encode())
+    return encrypted_data
+
+def scan_vulnerabilities(url):
+    vulners = Vulners(api_key=vulners_api_key)
+    vulnerabilities = vulners.webapp_scan(url)
+    return vulnerabilities
+
+def detect_waf(url):
+    wafw00f = WafW00F(url, ssl=True)
+    waf_detection = wafw00f.identify_waf()
+    return waf_detection
+
+def enumerate_subdomains(domain):
+    subdomains = SubBrute(domain).subnames()
+    return subdomains
+
+def test_xss_vulnerability(url):
+    # Send a POST request with the XSS payload as a parameter
+    response = requests.post(url, data={'input': xss_payload})
+    if xss_payload in response.text:
+        return True
+    return False
+
+def sqlmap_scan(url):
+    # Initialize sqlmap API
+    sqlmap_api.sqlmap_scan(url)
+
+def analyze_website(url):
+    # Fetch the website content
+    content = fetch_website_content(url)
+    if not content:
+        return
+
+    # Extract links from the website content
+    base_url = urlparse(url).scheme + '://' + urlparse(url).netloc
+    links = extract_links(content, base_url)
+    print(f"Found {len(links)} links on the website.")
+
+    # Calculate the MD5 hash of the website content
+    md5_hash = calculate_md5_hash(content)
+    print(f"MD5 Hash of the website content: {md5_hash}")
+
+    # Encrypt sensitive data
+    encryption_key = Fernet.generate_key()
+    sensitive_data = 'Sensitive information'
+    encrypted_data = encrypt_sensitive_data(sensitive_data, encryption_key)
+    print(f"Encrypted sensitive data: {encrypted_data}")
+
+    # Scan for vulnerabilities using Vulners API
+    with ThreadPoolExecutor() as executor:
+        results = executor.map(scan_vulnerabilities, links)
+    for link, vulnerabilities in zip(links, results):
+        print(f"Vulnerabilities for {link}:")
+        for vulnerability in vulnerabilities:
+            print(vulnerability)
+
+    # Detect Web Application Firewall (WAF)
+    waf_detection = detect_waf(url)
+    if waf_detection:
+        print(f"Detected WAF: {waf_detection}")
+    else:
+        print("No WAF detected.")
+
+    # Enumerate subdomains
+    parsed_url = urlparse(url)
+    domain = parsed_url.netloc.split(':')[0]
+    subdomains = enumerate_subdomains(domain)
+    print(f"Found {len(subdomains)} subdomains:")
+    for subdomain in subdomains:
+        print(subdomain)
+
+    # Test for XSS vulnerability
+    if test_xss_vulnerability(url):
+        print(f"The website is vulnerable to XSS attacks.")
+    else:
+        print("The website is not vulnerable to XSS attacks.")
+
+    # Perform SQL injection scan using sqlmap
+    sqlmap_scan(url)
+
+    # Perform additional security checks and analysis
+    # ...
+
+
+# Test the script
+analyze_website(website_url)
